49d3b325f5d643a2c9d26c8b5d07cf9ab9549eb6
David de Boer <david@ddeboer.nl>
2014-03-17 18:16:53 +0100

    Make parameters configurable through Puppet

--- manifests/parameters.pp
+++ manifests/parameters.pp
@@ -1,9 +1,26 @@
-define symfony::parameters ($original = "${name}.dist")
-{
+define symfony::parameters (
+  $template = 'symfony/parameters.yml.dist.erb',
+  $values   = {}
+) {
   Symfony::Parameters <| |> -> Symfony::Composer <| |>
 
+  # Defaults based on Symfony Standard Edition
+  $defaults = {
+    database_driver  => 'pdo_mysql',
+    database_host    => '127.0.0.1',
+    database_port    => '~',
+    mailer_transport => 'smtp',
+    mailer_host      => '127.0.0.1',
+    mailer_user      => '~',
+    mailer_password  => '~',
+    locale           => 'en',
+    secret           => 'ThisTokenIsNotSoSecretChangeIt',
+  }
+
+  $parameters = merge($defaults, $values)
+
   file { "${name}":
-      ensure => present,
-      content => template("$original"),
+    ensure  => present,
+    content => template($template),
   }
 }
