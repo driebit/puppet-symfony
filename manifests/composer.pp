17f669bd44e468a51a3c75c53e5aebcedfeaf310
David de Boer <david@ddeboer.nl>
2014-03-17 17:28:46 +0100

    Add no-interaction option (defaults to true)

--- manifests/composer.pp
+++ manifests/composer.pp
@@ -2,15 +2,21 @@
 define symfony::composer (
   $binary = 'composer',
   $dev    = true,
-  $user   = undef
+  $user   = undef,
+  $no_interaction = true,
 ) {
   $devOption = $dev ? {
     true    => '--dev',
     default => '',
   }
 
+  $interaction = $no_interaction ? {
+    true    => '--no-interaction',
+    default => ''
+  }
+
   exec { "composer install ${name}" :
-    command     => "${binary} install --verbose --working-dir ${name} ${devOption}",
+    command     => "${binary} install --verbose ${interaction} --working-dir ${name} ${devOption}",
     timeout     => 0,
     environment => "COMPOSER_HOME=/home/vagrant/.",
     path        => ['/usr/local/bin', '/usr/bin' ],
