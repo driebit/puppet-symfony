d0c88502dba79ee2ddf02da25dd254833569e1c7
David de Boer <david@ddeboer.nl>
2014-03-28 18:00:11 +0100

    Improve resource ordering

--- manifests/console.pp
+++ manifests/console.pp
@@ -2,7 +2,7 @@
 define symfony::console (
   $doc_root = '/vagrant'
 ) {
-  Symfony::Composer <| |> -> Symfony::Console <| |>
+  Symfony::Composer <| |> -> Symfony::Permissions <| |> -> Symfony::Parameters <| |> -> Symfony::Console <| |>
 
   exec { "${name}":
     command => "php ${doc_root}/app/console ${name}",
--- manifests/doctrine/fixtures.pp
+++ manifests/doctrine/fixtures.pp
@@ -2,6 +2,8 @@
 define symfony::doctrine::fixtures (
   $env  = 'dev'
 ) {
+  Symfony::Doctrine::Schema <| |> -> Symfony::Doctrine::Fixtures <| |>
+
   symfony::console { "doctrine:fixtures:load --no-interaction --purge-with-truncate -e=${env}":
     doc_root => $name
   }
