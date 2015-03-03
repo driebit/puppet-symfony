d3ae7e74d6c9dd06177a3878eb62755c7d2cc497
David de Boer <david@ddeboer.nl>
2015-03-03 17:39:08 +0100

    Allow defaults to be disabled

--- manifests/parameters.pp
+++ manifests/parameters.pp
@@ -1,23 +1,30 @@
 define symfony::parameters (
   $template = 'symfony/parameters.yml.dist.erb',
-  $values   = {}
+  $values   = {},
+  $defaults = true,
 ) {
   Symfony::Parameters <| |> -> Symfony::Composer <| |>
 
-  # Defaults based on Symfony Standard Edition
-  $defaults = {
-    database_driver  => 'pdo_mysql',
-    database_host    => '127.0.0.1',
-    database_port    => '~',
-    mailer_transport => 'smtp',
-    mailer_host      => '127.0.0.1',
-    mailer_user      => '~',
-    mailer_password  => '~',
-    locale           => 'en',
-    secret           => 'ThisTokenIsNotSoSecretChangeIt',
+  # Merge defaults
+  if $defaults {
+    # Defaults based on Symfony Standard Edition
+    $parameters = merge(
+      {
+        database_host    => '127.0.0.1',
+        database_driver  => 'pdo_mysql',
+        database_port    => '~',
+        mailer_transport => 'smtp',
+        mailer_host      => '127.0.0.1',
+        mailer_user      => '~',
+        mailer_password  => '~',
+        locale           => 'en',
+        secret           => 'ThisTokenIsNotSoSecretChangeIt',
+      },
+      $values
+    )
+  } else {
+    $parameters = $values
   }
-
-  $parameters = merge($defaults, $values)
 
   file { "${name}":
     ensure  => present,
